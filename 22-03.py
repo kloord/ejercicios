c0804cd5aa900079f210b2343868feed822311f2
kloord <98490134+kloord@users.noreply.github.com>
2022-03-22 17:04:23 -0300

    Add files via upload

---
+++ 22-03.py
@@ -0,0 +1,15 @@
+'''
+Calcula el IMC
+'''
+peso = float(input('Cual es tu peso?'))
+estatura = float(input('Cual es tu estatura?'))
+altura_c = estatura * estatura
+imc = float(peso / altura_c)
+if imc < 18.5:
+    print('Tu IMC es', imc, 'y tu indice de peso es bajo')
+elif imc < 24.9:
+    print('Tu IMC es', imc, 'y tu indice de peso es normal')
+elif imc < 29.9:
+    print('Tu IMC es', imc, 'y tu indice de peso es sobrepeso')
+elif imc >= 30.0:
+    print('Tu IMC es', imc, 'y tu indice de peso es sobrepeso')
