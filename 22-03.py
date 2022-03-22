f6a0cb3075db8ef4bdcc1570d43e5fa14a38fa91
kloord <98490134+kloord@users.noreply.github.com>
2022-03-22 17:12:35 -0300

    Update 22-03.py

--- 22-03.py
+++ 22-03.py
@@ -7,9 +7,9 @@ altura_c = estatura * estatura
 imc = float(peso / altura_c)
 if imc < 18.5:
     print('Tu IMC es', imc, 'y tu indice de peso es bajo')
-elif imc < 24.9:
+elif imc <= 24.9:
     print('Tu IMC es', imc, 'y tu indice de peso es normal')
-elif imc < 29.9:
+elif imc <= 29.9:
     print('Tu IMC es', imc, 'y tu indice de peso es sobrepeso')
 elif imc >= 30.0:
     print('Tu IMC es', imc, 'y tu indice de peso es sobrepeso')
